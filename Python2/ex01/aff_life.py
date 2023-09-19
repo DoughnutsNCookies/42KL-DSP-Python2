7924af331f178cb5282758cb2be78a7d2662229b
Chuah Tse Yung <105269435+DoughnutsNCookies@users.noreply.github.com>
2023-09-19 13:57:28 +0000

    ex01

---
+++ Python2/ex01/aff_life.py
@@ -0,0 +1,26 @@
+from load_csv import load
+import matplotlib.pyplot as plt
+
+
+def main():
+    """
+    Load the life expectancy dataset and plot the life expectancy of Malaysia
+    """
+    try:
+        data = load("life_expectancy_years.csv")
+        malaysia = data[data["country"] == "Malaysia"]
+        years = malaysia.columns[1:].astype(int)
+        life_expectancy = malaysia.values[0, 1:].astype(float)
+
+        plt.title("Life expectancy in Malaysia")
+        plt.xlabel("Year")
+        plt.ylabel("Life expectancy")
+        plt.plot(years, life_expectancy)
+        plt.savefig("life_expectancy_years.jpg")
+    except Exception as err:
+        print(err)
+        return
+
+
+if __name__ == "__main__":
+    main()
